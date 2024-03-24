6dcb84a7e254c770bcc4094b43d5dbce2fb8b7fb
VeronikaVaraksina1 <plaguemoon@gmail.com>
2024-03-24 19:20:15 +0200

    fix bug

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -41,7 +41,7 @@ const contactsSlice = createSlice({
       })
       .addCase(deleteContact.fulfilled, (state, action) => {
         state.loading = false;
-        state.items.filter(item => item.id !== action.payload.id);
+        state.items = state.items.filter(item => item.id !== action.payload.id);
       })
       .addCase(deleteContact.rejected, state => {
         state.loading = false;
